e62a2e20da404cf0d2ae60fe7112c8cc13eeac15
Bill Hunt <bill@krues8dr.com>
2016-03-09 21:17:13 -0500

    Adding pathType option.

--- tasks/bless.js
+++ tasks/bless.js
@@ -131,7 +131,8 @@ module.exports = function (grunt) {
 						numFiles: filesLength,
 						output: outPutfileName,
 						suffix: suffix,
-						linefeed: options.compress ? '' : grunt.util.linefeed
+						linefeed: options.compress ? '' : grunt.util.linefeed,
+						pathType: options.pathType
 					});
 
 					header += grunt.util.linefeed;
